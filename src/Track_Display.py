e8a3f9145acdb93e8b3a21efc2695b8cfb374be4
junxi <baojunxivital@icloud.com>
2025-03-03 17:48:37 +0800

    添加人脸识别页面

--- src/Track_Display.py
+++ src/Track_Display.py
@@ -4,31 +4,19 @@ import libcamera
 from adafruit_servokit import ServoKit
 import time
 
-step = 1
-# 初始化伺服电机控制
-kit = ServoKit(channels=16)
-temp = 0
-temp1 = 0
-temp2 = 0
-temp3 = 0
-temp4 = 0
-# 初始伺服角度
-pan = 90
-tilt = 90
-kit.servo[0].angle = pan
-kit.servo[1].angle = tilt
-
-# 摄像头分辨率
-dispW, dispH = 640, 480
-
-# 人脸检测模型加载
-face_cascade = cv2.CascadeClassifier('../models/haarcascade_frontalface_default.xml')
-
-
-def Track_Display():
+def track_display(dispW=640, dispH=480, step=1, model_path='../models/haarcascade_frontalface_default.xml', output_path='./static/image/output.jpeg'):
     """摄像头视频采集与人脸检测"""
-    global pan, tilt,temp1,temp2,temp3,temp4,temp
-
+    # 初始化伺服电机控制
+    kit = ServoKit(channels=16)
+    pan, tilt = 90, 90
+    kit.servo[0].angle = pan
+    kit.servo[1].angle = tilt
+    
+    temp, temp1, temp2, temp3, temp4 = 0, 0, 0, 0, 0
+    
+    # 人脸检测模型加载
+    face_cascade = cv2.CascadeClassifier(model_path)
+    
     # 初始化摄像头
     picamera = Picamera2()
     config = picamera.create_preview_configuration(
@@ -38,83 +26,76 @@ def Track_Display():
     config["transform"] = libcamera.Transform(hflip=1, vflip=1)
     picamera.configure(config)
     picamera.start()
-
+    
     try:
         while True:
-            # 捕获帧
-            time.sleep(0.15)
             frame = picamera.capture_array()
             frame[..., [0, 2]] = frame[..., [2, 0]]  # B 和 R 通道交换
-
+            
             # 转换为灰度图
             gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-            faces = face_cascade.detectMultiScale(gray, 1.3, 15)
-
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            
             # 人脸检测与伺服控制
             for (x, y, w, h) in faces:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                Xcent = x + w / 2
-                Ycent = y + h / 2
+                Xcent, Ycent = x + w / 2, y + h / 2
                 
-
-                # 限制伺服角度范围
                 pan = max(0, min(180, pan))
                 tilt = max(0, min(180, tilt))
-
-                if (Xcent - dispW / 2) >=35:
-                   pan -=  step
+                
+                if (Xcent - dispW / 2) >= 35:
+                    pan -= step
                 else:
                     temp1 = 1
-
-                if (Xcent - dispW / 2) <=-35:
-                   pan +=  step
+                
+                if (Xcent - dispW / 2) <= -35:
+                    pan += step
                 else:
                     temp2 = 1
-
+                
                 if (Ycent - dispH / 2) >= 35:
-                   tilt +=  step
+                    tilt += step
                 else:
                     temp3 = 1
-
+                
                 if (Ycent - dispH / 2) <= -35:
-                   tilt -=  step
+                    tilt -= step
                 else:
                     temp4 = 1
-
-                if temp1 ==1 and temp2 == 1 and temp3 == 1 and temp4 == 1:
+                
+                if temp1 == 1 and temp2 == 1 and temp3 == 1 and temp4 == 1:
                     temp = 1
-
+                
                 if temp == 1:
                     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                    print ("拍摄中～")
-                    cv2.imwrite("../data/output.jpeg",frame)
-                    print ("图片拍摄完成！")
+                    print("拍摄中～")
+                    cv2.imwrite(output_path, frame)
+                    print("图片拍摄完成！")
                     break
                 
                 kit.servo[0].angle = pan
                 kit.servo[1].angle = tilt
-                #//time.sleep(0.1)
-
+                
             if temp == 1:
                 time.sleep(0.5)
                 break
-            else: 
-                temp1 = 0
-                temp2 = 0
-                temp3 = 0
-                temp4 = 0
-
-
+            else:
+                temp1, temp2, temp3, temp4 = 0, 0, 0, 0
+                
             # 显示图像
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            cv2.imshow("Camera Output", frame)
-
+            cv2.imwrite("./static/image/temp.jpeg", frame)
+            
             # 按 'q' 退出
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
-
+    
     finally:
         # 释放资源
         picamera.stop()
         cv2.destroyAllWindows()
+
+# 调用函数
+if __name__ == "__main__":
+    track_display()
--- src/webUI.py
+++ src/webUI.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
+import threading
+import time
+import Track_Display
 
 app = Flask(__name__)
+isStart = False
 
 # 首页路由
 @app.route('/')
 def home():
     lang = request.args.get('lang', 'zh')  # 获取URL中的语言参数，默认为中文
     return render_template('home.html', lang=lang)
+
+
 
 # 关于页面路由
 @app.route('/about')
@@ -19,6 +25,17 @@ def introduction():
     lang = request.args.get('lang', 'zh')  # 获取URL中的语言参数，默认为中文
     return render_template('introduction.html', lang=lang)
 
+@app.route('/face')
+def face():
+    global isStart
+    lang = request.args.get('lang', 'zh')
+    if isStart == False:
+        tread = threading.Thread(target=Track_Display.track_display)
+        tread.start()
+        isStart = True
+    
+    return render_template('faceRecognition.html', lang=lang)
+
 if __name__ == '__main__':
     # 启动Flask服务并监听所有可用的IP地址，这样在局域网中其他设备可以访问
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
